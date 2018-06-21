4d0b3c24aa2221fc664017370f72a0123ccada68
Jared Youtsey <jared.youtsey@jbhunt.com>
2018-06-21 16:13:23 -0500

    hide subject

--- src/app/common/github-service/github.service.ts
+++ src/app/common/github-service/github.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import { first } from 'rxjs/operators/first';
 import { isNumeric } from 'rxjs/util/isNumeric';
@@ -8,9 +9,13 @@ import { GithubResponse } from './github-response.interface';
 @Injectable()
 export class GithubService {
 
-  angularVersionSubject$: Subject<string> = new Subject<string>();
+  angularVersionSubject$: Observable<string>;
 
-  constructor(private http: HttpClient) { }
+  private angularVersionSubject: Subject<string> = new Subject<string>();
+
+  constructor(private http: HttpClient) {
+    this.angularVersionSubject$ = this.angularVersionSubject.asObservable();
+  }
 
   getAngularLatestVersion() {
     this.http.get<GithubResponse[]>('https://api.github.com/repos/angular/angular/tags')
@@ -19,7 +24,7 @@ export class GithubService {
         const latest = response.find((item: GithubResponse) => {
           return isNumeric(item.name.substring(0, 1));
         });
-        this.angularVersionSubject$.next(latest ? latest.name : undefined);
+        this.angularVersionSubject.next(latest ? latest.name : undefined);
       });
   }
 }
